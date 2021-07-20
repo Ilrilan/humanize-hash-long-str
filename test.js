a5896b53fdb01e4b2dc39edd3a895f03edb83445
Mikhail Shilov <ext.mshilov@tinkoff.ru>
2021-07-20 14:55:40 +0500

    Add npm ignore

--- test.js
+++
@@ -1,5 +0,0 @@
-const { humanizeHashLongStr } = require('./humanize-hash-long-str')
-
-const longStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghjijklmnopqrstuvwxyz'
-
-console.log(humanizeHashLongStr(longStr, { maxLength: 30, humanizeHash: false, length: 5 }))
